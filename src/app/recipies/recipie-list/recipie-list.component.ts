afb5b2188296b58e89d1f037ec38d14949e05b85
Dipu Divakaran <Dipu_Divakaran@ryder.com>
2019-05-30 12:56:15 -0400

    commiting my changes

--- src/app/recipies/recipie-list/recipie-list.component.ts
+++ src/app/recipies/recipie-list/recipie-list.component.ts
@@ -2,6 +2,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Recipie } from 'src/app/model/recipie.model';
 import { RecipieService } from 'src/app/services/recipie.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipie-list',
@@ -10,12 +11,22 @@ import { RecipieService } from 'src/app/services/recipie.service';
 })
 export class RecipieListComponent implements OnInit {
   recipies: Recipie[];
+  colors: any;
+  sub: Subscription;
+  color: any;
   constructor(private recipieService: RecipieService,
               private router: Router,
               private route: ActivatedRoute
               ) { }
 
   ngOnInit() {
+    this.color = [];
+    if(this.color.length === 0) {
+      this.router.navigate(['/recipies']);
+    }
+    this.colors = [{color:'yellow',isChecked:false},
+    {color:'pink',isChecked:false},
+    {color:'blue', isChecked:false}];
     this.recipieService.recipiedChanged.subscribe(
       (recipies: Recipie[]) => {
         this.recipies = recipies;
@@ -25,5 +36,31 @@ export class RecipieListComponent implements OnInit {
   }
   onNewRecipe() {
     this.router.navigate(['new'], {relativeTo: this.route});
+  }
+
+  radioClicked(radioObj) {
+    this.color = [];
+    this.colors.forEach(rad => {
+      if (rad.color === radioObj.color) {
+        rad.isChecked = !rad.isChecked;
+      }
+      if (rad.isChecked) {
+        if ((this.color.toString().search(rad.color) === -1)) {
+          this.color.push([rad.color]);
+        }
+        this.router.navigate(['/recipies'], { queryParams: { color: this.color } });
+      } else {
+        if ((this.color.toString().search(rad.color) !== -1)) {
+          this.color.splice(this.color.indexOf([rad.color]), 1);
+        }
+        if (this.color.length !== 0) {
+          this.router.navigate(['/recipies'], { queryParams: { color: this.color } });
+        } else {
+          this.router.navigate(['/recipies']);
+        }
+      }
+    });
+    console.log(this.color);
+    console.log(location.href);
   }
 }
